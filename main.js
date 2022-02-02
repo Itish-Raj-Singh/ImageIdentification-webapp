af18e01a4526ee86dc6306fc2438c4635dca2b12
Itish-Raj-Singh <94170586+Itish-Raj-Singh@users.noreply.github.com>
2022-02-02 16:34:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+width:350,
+height:300,
+image_format:"png",
+png_quality:90
+});
+
+ Webcam.attach('#camera');
+
+ function take_snapshot(){
+   Webcam.snap(function(data_uri){
+       document.getElementById("result").innerHTML ='<img id="captured_image" src="'+data_uri+'"/>';
+   })
+ }
+
+ console.log("ml5 version" , ml5.verion);
+
+camera = document.getElementById("camera");
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/o7kiXAlbB/model.json',modelloaded)
+
+function modelloaded(){
+console.log('modelloaded');
+}
+
+function check(){
+  img = document.getElementById('captured_image');
+  classifier.classify(img , gotResult);
+}
+ function gotResult(error , result){
+   if(error){
+   console.error(error);
+  }
+  else{
+    console.log(result);
+    document.getElementById("results_of_object").innerHTML = result[0].label;
+    document.getElementById("results_of_Accuracy").innerHTML= result[0].confidence.toFixed(3);
+  }
+  }
